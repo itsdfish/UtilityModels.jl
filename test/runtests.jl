498570f67ed5a07167babbe132021a5604bd8aa8
itsdfish <itsdfish@gmail.com>
2021-02-10 09:19:08 -0500

    fix versions

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using SafeTestsets
     # test values based on http://psych.fullerton.edu/mbirnbaum/calculators/cpt_calculator.htm
     using Test, UtilityModels
     import UtilityModels: compute_weights
-    α=.8; β=.9; γg=.6; γl=.7; λ=2.25
+    α = .8; β = .9; γg = .6; γl =.7; λ = 2.25
     model = ProspectTheory(;α, β, γg, γl, λ)
     p = [.3,.2,.3,.2]
     v = [10.0,3.0,-2.0,-1.0]
@@ -16,7 +16,7 @@ using SafeTestsets
     ωl = compute_weights(gamble.pl, γl)
     @test ωl ≈ [0.1293,0.3281] atol = 1e-4
 
-    α=1.2; β=1.5; γg=.8; γl=.9; λ=1.5
+    α = 1.2; β = 1.5; γg = .8; γl = .9; λ = 1.5
     model = ProspectTheory(;α, β, γg, γl, λ)
     p = [.1,.2,.5,.2]
     v = [1.0,3.0,-5.0,-2.0]
