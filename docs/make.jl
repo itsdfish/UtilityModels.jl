4f7f2f898554720594aedb68f78f284e02182da9
itsdfish <itsdfish@gmail.com>
2024-07-27 14:24:41 -0400

    fix makedocs.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,27 @@ using Documenter
 using UtilityModels
 
 makedocs(
-    sitename = "UtilityModels",
-    format = Documenter.HTML(),
-    modules = [UtilityModels]
+    warnonly = true,
+    sitename="UtilityModels",
+    format=Documenter.HTML(
+        assets=[
+            asset(
+                "https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap",
+                class=:css,
+            ),
+        ],
+        collapselevel=1,
+    ),
+    modules=[
+        UtilityModels, 
+        # Base.get_extension(SequentialSamplingModels, :TuringExt),  
+        # Base.get_extension(SequentialSamplingModels, :PlotsExt) 
+    ],
+    pages=[
+        "Home" => "index.md",
+    ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo="github.com/itsdfish/UtilityModels.jl.git",
+)
