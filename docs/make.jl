cfd719e0a763cf3044df60a36be118e2a52603f6
itsdfish <9677184+itsdfish@users.noreply.github.com>
2024-07-27 18:26:57 +0000

    Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -7,16 +7,16 @@ makedocs(
     format = Documenter.HTML(
         assets = [
             asset(
-                "https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap",
-                class = :css
-            )
+            "https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap",
+            class = :css
+        )
         ],
         collapselevel = 1
     ),
     modules = [
         UtilityModels
-        # Base.get_extension(SequentialSamplingModels, :TuringExt),  
-        # Base.get_extension(SequentialSamplingModels, :PlotsExt) 
+    # Base.get_extension(SequentialSamplingModels, :TuringExt),  
+    # Base.get_extension(SequentialSamplingModels, :PlotsExt) 
     ],
     pages = [
         "Home" => "index.md"
